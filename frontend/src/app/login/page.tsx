b8b72f6bc8a7a6502c94f0b9f8d720de28276478
Dhyan V. <107373897+FlmnD@users.noreply.github.com>
2025-12-10 12:23:24 -0600

    Update Deprecated Properties

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,6 +1,6 @@
 export default function Login() {
     return (
-        <section className="min-h-[calc(100dvh-3.5rem)] bg-gradient-to-b from-sky-50 via-white to-white">
+        <section className="min-h-[calc(100dvh-3.5rem)] bg-linear-to-b from-sky-50 via-white to-white">
             <h1 className="text-2xl font-semibold text-black">Login</h1>
             <p className="mt-2 text-gray-600">
                 Login to hour account here.
--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,6 +1,6 @@
 export default function SignUp() {
     return (
-        <section className="min-h-[calc(100dvh-3.5rem)] bg-gradient-to-b from-sky-50 via-white to-white">
+        <section className="min-h-[calc(100dvh-3.5rem)] bg-linear-to-b from-sky-50 via-white to-white">
             <h1 className="text-2xl font-semibold text-black">Sign Up</h1>
             <p className="mt-2 text-gray-600">
                 Create your account here.
